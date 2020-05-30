c7146d52be384fbaa5d5f27b326b9da438938511
leoarmelin <leonardo99.armelin@gmail.com>
2020-05-30 04:58:18 -0300

    v1.10

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -185,17 +185,37 @@ function Home() {
         <div className='projectTopView' style={{ height: height/12, width: width }}>
           <p className='doneTitle' style={{ fontSize: height/17 }}>CONTATO</p>
         </div>
-        <div style={{ height: height*(6/8), width: width, backgroundColor: '#aaa' }}>
-        <Iframe 
-          src={urlMap}
-          frameborder="0"
-          style={{ border:0 }}
-          allowfullscreen="" 
-          aria-hidden="false" 
-          tabindex="0"
-          width="640" 
-          height="480">
-        </Iframe>
+        <div className='contactBottomView' style={{ height: height*(6/8), width: width }}>
+          <div className='contactBottomLeftView'>
+            <div className='contactSubtitleView'>
+              <p className='contactSubtitleText'>FALE CONOSCO</p>
+            </div>
+            <textarea className='contactEmailInput'
+            type='text'
+            placeholder='Digite seu e-mail'
+            ></textarea>
+            <textarea className='contactMessageInput'
+            placeholder='Nos envie uma mensagem!'></textarea>
+            <button className='contactSendButton'
+            value='Enviar'>Enviar</button>
+          </div>
+          <div className='contactBottomRightView'>
+            <div className='corner-wrapper'>
+              <Iframe
+                src={urlMap}
+                frameborder="0"
+                style={{ border: 0 }}
+                allowfullscreen="" 
+                aria-hidden="false" 
+                tabindex="0"
+                width='100%'
+                height='100%'>
+              </Iframe>
+            </div>
+            <p className='contactBottomRightText' style={{ marginTop: '2%' }}>Universidade Estadual de Maringá</p>
+            <p className='contactBottomRightText'>Avenida Colombo, 5970</p>
+            <p className='contactBottomRightText'>ieeeuem@gmail.com</p>
+          </div>
         </div>
       </footer>
 
