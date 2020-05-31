8d588370c4e5dd3b44711fdb94d1fc99c41a5e0e
leonichel <leonichelg@gmail.com>
2020-05-31 04:26:32 -0300

    v1.13 - home fix + add admin

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './User.css';
 
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
 function User() {
   return (
     <div className="User">
-      
+      <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+    	</Admin>
     </div>
   );
 }
