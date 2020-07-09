e4928bd104c858ed09aa32e8ce3eba2f39cbca59
unknown <rodolfo_fero@hotmail.com>
2020-07-09 17:11:11 -0300

    v1.4.8

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
+const https = require('https');
 
 const api = axios.create({
     baseURL: 'https://backendieee.herokuapp.com/',
-
+    httpsAgent: new https.Agent({  
+        rejectUnauthorized: false
+    })
 })
 
 export default api
