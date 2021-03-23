65aaafb1ee5626200fa5e5c4fd3d6ed565ae0afc
Rodolfo Lemes <rodolfo_fero@hotmail.com>
2021-03-23 11:30:33 -0300

    feat: add seel and prettier

---
+++ src/pages/Seel/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+function Seel() {
+	return (
+		<Container>
+			<h1>Seel</h1>
+		</Container>
+	);
+}
+
+export default Seel;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { DimensionProvider } from './context/dimension'
+import { DimensionProvider } from './context/dimension';
 
 import Archives from './pages/Archives/Archives';
 import Blog from './pages/Blog/Blog';
@@ -10,23 +10,25 @@ import Home from './pages/Home/Home';
 import Store from './pages/Store/Store';
 import Forms from './pages/Forms/Forms';
 import User from './pages/User/User';
+import Seel from './pages/Seel';
 
 function Routes() {
-    return(
-        <DimensionProvider>
-            <BrowserRouter>
-                <Switch>
-                    <Route path='/' exact={true} component={Home} />
-                    <Route path='/archives' component={Archives} />
-                    <Route path='/blog' component={Blog} />
-                    <Route path='/events' component={Events} />
-                    <Route path='/store' exact={true} component={Store} />
-                    <Route path='/store/:course' component={Forms} />
-                    <Route path='/user' component={User} />
-                </Switch>
-            </BrowserRouter>
-        </DimensionProvider> 
-    )
+	return (
+		<DimensionProvider>
+			<BrowserRouter>
+				<Switch>
+					<Route path="/" exact={true} component={Home} />
+					<Route path="/archives" component={Archives} />
+					<Route path="/blog" component={Blog} />
+					<Route path="/events" component={Events} />
+					<Route path="/store" exact={true} component={Store} />
+					<Route path="/store/:course" component={Forms} />
+					<Route path="/user" component={User} />
+					<Route path="/seel" component={Seel} />
+				</Switch>
+			</BrowserRouter>
+		</DimensionProvider>
+	);
 }
 
-export default Routes
+export default Routes;
