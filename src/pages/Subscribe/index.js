9fdcad341fb570926e9546c4ba7978643d94352c
leoarmelin <leonardo99.armelin@gmail.com>
2021-03-25 16:16:28 -0300

    feat: added useStates in Subscribe. fix: brought FormSelector to Subscribe

--- src/pages/Subscribe/index.js
+++ src/pages/Subscribe/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	Container,
 	HeaderTitle,
@@ -7,12 +7,28 @@ import {
 	FinishButton,
 	InfoText,
 	ColoredText,
+	FormSelectorContainer,
+	FormSelectorLabel,
+	OptionsContainer,
+	Option,
+	OptionText,
 } from './styles';
 
 import FormInput from './components/FormInput';
-import FormSelector from './components/FormSelector';
 
 function Subscribe() {
+	const [nameInput, setNameInput] = useState(null);
+	const [addressInput, setAddressInput] = useState(null);
+	const [institutionInput, setInstitutionInput] = useState(null);
+	const [cityInput, setCityInput] = useState(null);
+	const [cepInput, setCepInput] = useState(null);
+	const [phoneInput, setPhoneInput] = useState(null);
+	const [cpfInput, setCpfInput] = useState(null);
+	const [rgInput, setRgInput] = useState(null);
+	const [emailInput, setEmailInput] = useState(null);
+	const [membershipSelector, setMembershipSelector] = useState(null);
+	const [membershipInput, setMembershipInput] = useState(null);
+	const [productSelector, setProductSelector] = useState(null);
 	return (
 		<Container>
 			<HeaderTitle>Inscrição</HeaderTitle>
@@ -23,11 +39,13 @@ function Subscribe() {
 						labelText="Nome"
 						placeholderText="Nome completo"
 						rowWidth={48}
+						onchange={() => setNameInput(nameInput)}
 					/>
 					<FormInput
 						labelText="Endereço"
 						placeholderText="Rua, número"
 						rowWidth={48}
+						onchange={() => setAddressInput(addressInput)}
 					/>
 				</RowContainer>
 
@@ -36,11 +54,13 @@ function Subscribe() {
 						labelText="Instituição de ensino"
 						placeholderText="Ex: Universidade Estadual de Maringá"
 						rowWidth={48}
+						onchange={() => setInstitutionInput(institutionInput)}
 					/>
 					<FormInput
 						labelText="Cidade"
 						placeholderText="Ex: Maringá"
 						rowWidth={48}
+						onchange={() => setCityInput(cityInput)}
 					/>
 				</RowContainer>
 
@@ -48,17 +68,23 @@ function Subscribe() {
 					<FormInput
 						labelText="CEP"
 						placeholderText="XXXXX-XXX"
+						type="number"
 						rowWidth={31}
+						onchange={() => setCepInput(cepInput)}
 					/>
 					<FormInput
 						labelText="Telefone"
 						placeholderText="(XX) X XXXX-XXXX"
+						type="tel"
 						rowWidth={31}
+						onchange={() => setPhoneInput(phoneInput)}
 					/>
 					<FormInput
 						labelText="CPF"
 						placeholderText="XXX.XXX.XXX-XX"
+						type="number"
 						rowWidth={31}
+						onchange={() => setCpfInput(cpfInput)}
 					/>
 				</RowContainer>
 
@@ -66,33 +92,63 @@ function Subscribe() {
 					<FormInput
 						labelText="RG"
 						placeholderText="XX.XXX.XXX-X"
+						type="number"
 						rowWidth={31}
+						onchange={() => setRgInput(rgInput)}
 					/>
 					<FormInput
 						labelText="E-mail"
 						placeholderText="seuemail@dominio.com"
+						type="email"
 						rowWidth={65}
+						onchange={() => setEmailInput(emailInput)}
 					/>
 				</RowContainer>
 
 				<RowContainer>
-					<FormSelector
-						labelText="Possui membresia?"
-						rowWidth={31}
-						leftText="Sim"
-						rightText="Não"
-					/>
+					<FormSelectorContainer rowWidth={31}>
+						<FormSelectorLabel>Possui membresia?</FormSelectorLabel>
+						<OptionsContainer>
+							<Option
+								type="radio"
+								checked={membershipSelector}
+								onClick={() => setMembershipSelector(true)}
+							/>
+							<OptionText>Sim</OptionText>
+							<Option
+								type="radio"
+								checked={!membershipSelector}
+								onClick={() => setMembershipSelector(false)}
+							/>
+							<OptionText>Não</OptionText>
+						</OptionsContainer>
+					</FormSelectorContainer>
+
 					<FormInput
 						labelText="Membresia"
 						placeholderText="XXXXXXXX"
+						type="number"
 						rowWidth={31}
+						onchange={() => setMembershipInput(membershipInput)}
 					/>
-					<FormSelector
-						labelText="Escolha o produto"
-						rowWidth={31}
-						leftText="Evento"
-						rightText="Evento + kit"
-					/>
+
+					<FormSelectorContainer rowWidth={31}>
+						<FormSelectorLabel>Escolha o produto</FormSelectorLabel>
+						<OptionsContainer>
+							<Option
+								type="radio"
+								checked={productSelector}
+								onClick={() => setProductSelector(true)}
+							/>
+							<OptionText>Evento</OptionText>
+							<Option
+								type="radio"
+								checked={!productSelector}
+								onClick={() => setProductSelector(false)}
+							/>
+							<OptionText>Evento + kit</OptionText>
+						</OptionsContainer>
+					</FormSelectorContainer>
 				</RowContainer>
 			</FormContainer>
 
--- src/pages/Subscribe/styles.js
+++ src/pages/Subscribe/styles.js
@@ -95,3 +95,33 @@ export const InfoText = styled.text`
 export const ColoredText = styled.text`
 	color: ${colors.yellow};
 `;
+
+export const FormSelectorContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	height: auto;
+	width: ${props => `${props.rowWidth}%`};
+	margin-top: 20px;
+	padding-left: 10px;
+`;
+
+export const FormSelectorLabel = styled.label`
+	font-size: 20px;
+	font-family: Poppins-regular;
+	color: ${colors.white};
+`;
+
+export const OptionsContainer = styled.div`
+	display: flex;
+	height: 50px;
+	align-items: center;
+`;
+
+export const Option = styled.input``;
+
+export const OptionText = styled.text`
+	font-size: 18px;
+	font-family: Poppins-regular;
+	margin: 0px 20px 0px 10px;
+	color: ${colors.white};
+`;
