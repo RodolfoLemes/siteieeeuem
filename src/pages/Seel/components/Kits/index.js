7c0b33a39233b93eb790c0768be39a21aefc9f77
Rodolfo Lemes <rodolfo_fero@hotmail.com>
2021-03-28 12:05:05 -0300

    fix: name in kits components

--- src/pages/Seel/components/Kits/index.js
+++ src/pages/Seel/components/Kits/index.js
@@ -1,14 +1,14 @@
 import React, { forwardRef } from 'react';
 import { Container } from './styles';
 
-import PanelistsFlatList from './components/KitsFlatList';
+import KitsFlatList from './components/KitsFlatList';
 
 import kits from './descriptions';
 
 const Kits = forwardRef((props, ref) => {
 	return (
 		<Container ref={ref}>
-			<PanelistsFlatList data={kits} />
+			<KitsFlatList data={kits} />
 		</Container>
 	);
 });
