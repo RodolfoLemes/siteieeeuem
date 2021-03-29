f95c8c6591c239feda9501784b3303233d828a81
Rodolfo Lemes <rodolfo_fero@hotmail.com>
2021-03-29 00:56:52 -0300

    feat: add seel subscribers providers in to admin layout

--- src/utils/seelApi.js
+++ src/utils/seelApi.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
 	baseURL: 'https://seel-backend.herokuapp.com/',
+	headers: {
+		'Content-Type': 'application/json',
+		'Access-Control-Allow-Origin': '*',
+	},
 });
 
 export default api;
